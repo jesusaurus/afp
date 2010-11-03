45a1e33d1d1c00bdace3fd41aaaefd145f42cffd
Eric O'Connell <eric.oconnell@gmail.com>
2010-11-02 20:06:37 -0700

    used brutal hack to get sample data; made stupid ascii visulization thereof

---
+++ piping/readout.c
@@ -0,0 +1,87 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <libavformat/avformat.h>
+#include <libavcodec/avcodec.h>
+#include <libavutil/avutil.h>
+#include <libavutil/mathematics.h>
+
+#ifdef HAVE_AV_CONFIG_H
+#undef HAVE_AV_CONFIG_H
+#endif
+
+#define AUDIO_INBUF_SIZE 20480
+#define AUDIO_REFILL_THRESH 4096
+
+#ifndef FIGHTCLUB_WRITEOUT
+#define FIGHTCLUB_WRITEOUT
+
+int WriteOut(char* file) {
+    AVCodec *codec;
+    AVCodecContext *cContext = NULL;
+    //AVFormatContext *fContext = NULL;
+    AVPacket packet;
+    int frame_size, out_size;
+    short *samples;
+    //float t, tincr;
+    uint8_t *outbuf;
+    FILE *fout;
+
+    //standard initializations
+    av_register_all();
+    avcodec_init();
+    avcodec_register_all();
+    
+    av_init_packet(&packet);
+
+    //load the codec
+    codec = avcodec_find_encoder(CODEC_ID_MP3); //TODO: get ogg/flac/${opensourcecodec} working
+    if (!codec){
+        fprintf(stderr, "CODEC_ID_MP3 not found.");
+        return 1;
+    }
+
+    //create the codec context
+    cContext = avcodec_alloc_context();
+    cContext->bit_rate = 128000;
+    cContext->sample_rate = 44100;
+    cContext->channels = 2;
+    if (avcodec_open(cContext, codec) < 0) {
+        fprintf(stderr, "Failed to open codec.");
+        return 1;
+    }
+
+    frame_size = cContext->frame_size;
+    samples = malloc(frame_size * 2 * cContext->channels);
+    outbuf = malloc(AVCODEC_MAX_AUDIO_FRAME_SIZE);
+   
+    /*
+    //create the format context
+    fContext = avformat_alloc_context();
+    */
+
+    fout = fopen(file, "wb");
+    if (!fout) {
+        fprintf(stderr, "Error opening output file\n");
+        return 1;
+    }
+
+    //read stdin to the buffer, then out to file
+    do {
+        fread(samples, 1, frame_size, stdin);
+        out_size = avcodec_encode_audio(cContext, outbuf, AVCODEC_MAX_AUDIO_FRAME_SIZE, samples);
+        fwrite(outbuf, 1, out_size, fout);
+    } while (!feof(stdin) && !ferror(stdin));
+
+    //clean up
+    fclose(fout);
+    free(outbuf);
+    free(samples);
+
+    avcodec_close(cContext);
+    av_free(cContext);
+
+    return 0;
+}
+
+#endif
---
+++ src/main.go
@@ -0,0 +1,36 @@
+package main;
+
+import (
+	"libav"
+	"unsafe"
+)
+
+func main() {
+	var context libav.AVDecodeContext
+	libav.InitDecoding()
+	libav.PrepareDecoding("/tmp/test.mp3", &context)
+	for l := libav.DecodePacket(context); l > 0; {
+/*		println("Packet decode: ", l, " bytes")*/
+		l = libav.DecodePacket(context)
+		sample := (*(*[1 << 31 - 1]int16)(unsafe.Pointer(context.Context.Outbuf)))[:l/2]
+
+/*		for i := 0; i < l/2; i++ {
+			print(sample[i], " ")
+		}
+*/		for j,s := range sample {
+			if (j % 2 == 0) {
+				width := 236
+				half := int16(width/2) 
+				offset := int16(float(s) * float(width) / 65535.0)
+
+				for i := 0; i < int(half + offset); i++ {
+					print(" ")
+				}
+				println("#")
+			}
+		}
+/*		println("Decoded ", l, " bytes, oh joy!")
+		println("Context shows a packet at: ", []int16(reflect.MakeSlice(unsafe.Pointer(context.Context.Outbuf))))*/
+	}
+	
+}
