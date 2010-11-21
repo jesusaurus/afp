fb569a7c1160bc05f0c06e2b5e6f77fd206a5a52
Cory Kolbeck <ckolbeck@cs.pdx.edu>
2010-11-20 21:10:23 -0800

    Add draft types definition

---
+++ setup/types.go
@@ -0,0 +1,33 @@
+package afp
+
+import (
+	"os"
+)
+
+//Constants to specify the type of a given filter
+const (
+	PIPE_SOURCE = iota
+	PIPE_SINK
+	PIPE_LINK
+)
+
+type StreamHeader struct {
+	Version int
+	Channels int
+	SampleSize int
+	SampleRate int
+	ContentLength int64
+}
+
+type Context struct {
+	HeaderSource <-chan StreamHeader
+	HeaderSink chan<- StreamHeader
+	Source <-chan []byte
+	Sink chan<- []byte
+}
+
+type Filter interface {
+	GetType() int
+	Init(*Context, []string) os.Error
+	Start()
+}
