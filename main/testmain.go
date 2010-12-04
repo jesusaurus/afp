19810b1aa5fed90def2fdd81043cd4a9bcbd7184
Cory Kolbeck <ckolbeck@cs.pdx.edu>
2010-12-04 00:23:27 -0800

    Polish startup routines/filter testbed

--- main/testmain.go
+++ main/testmain.go
@@ -1,1 +1,36 @@
 package main
+
+import (
+	"afp"
+	"os/signal"
+	)
+
+type FilterWrapper {
+	filter afp.Filter
+	name string
+	finished chan int
+}
+
+var pipespec [][]string = [][]string{{"source"},{"sink"}}
+
+var (
+	Pipeline []*FilterWrapper = make(*FilterWrapper, 0, 100)
+	
+	errors log.Logger = log.New(os.Stderr, "[E] ", log.Ltime)
+	info log.Logger = log.New(os.Stderr, "[I] ", log.Ltime)
+	verbose bool = false
+	)
+
+
+func main() {
+	InitPipeline(pipespec)
+	StartPipeline()
+
+	for _, filter := range Pipeline {
+		<-filter.finished
+		if verbose {
+			info.Printf("Filter '%s' finished", filter.name)
+		}
+	}
+}
+
