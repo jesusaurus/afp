29e65ef34b6e9a7beec83efe2ada65f596fa666b
Jonathan Harker <kjharke@cs.pdx.edu>
2010-12-03 19:13:10 -0800

    Fleshed out some of AlsaSource

--- filters/alsa.go
+++ filters/alsa.go
@@ -11,7 +11,6 @@ import (
 // #include <alsa/asoundlib.h>
 import "C"
 
-
 /////
 // Alsa Source
 // Listens to a microphone
@@ -26,12 +25,30 @@ func (self *AlsaSource) GetType() int {
 
 func (self *AlsaSource) Init(ctx *afp.Context, args []string) os.Error {
     self.ctx = ctx
-    header <-ctx.HeaderSource
-    return nil
+
+    header := StreamHeader {
+        Version: 1
+        Channels: 1
+        SampleSize: 32
+        SampleRate: 44100
+    }
+
+    return self.prepare()
 }
 
 func (self *AlsaSource) Start() {
-    return
+    var buf *[]float32
+
+    for {
+        errno := C.snd_pcm_readn(capture, unsafe.Pointer(buf), 512)
+        if errno < 512 {
+            errtwo := C.snd_pcm_recover(capture, C.int(errno), 0);
+            if errtwo < 0 {
+                fmt.Println( "While reading from ALSA device, failed to recover from error: ", errtwo)
+                panic
+            }
+        }
+    }
 }
 
 /////
@@ -48,9 +65,7 @@ func (self *AlsaSink) GetType() int {
 
 func (self *AlsaSink) Init(ctx *afp.Context, args []string) os.Error {
     self.ctx = ctx
-
     header <-ctx.HeaderSource
-
     return self.prepare()
 }
 
@@ -60,7 +75,7 @@ func (self *AlsaSink) Start() {
         errno := C.snd_pcm_writen(playback, unsafe.Pointer(buffer), length)
 
         if errno < length {
-            return //not all the data was written
+            panic //not all the data was written
         }
     }
 }
@@ -82,33 +97,83 @@ func (self *AlsaSink) prepare() {
     }
 
     if errno := C.snd_pcm_hw_params_any(playback, params); errno < 0 {
-        return os.NewError( fmt.Printf("Could not initialize hardware parameter structure. Error %d", errno) )
+        return os.NewError( fmt.Sprintf("Could not initialize hardware parameter structure. Error %d", errno) )
     }
 
     if errno := C.snd_pcm_hw_params_set_access (playback, params, C.SND_PCM_ACCESS_RW_INTERLEAVED); errno < 0 {
-        return os.NewError( fmt.Printf("Could not set access type. Error %d", errno) )
+        return os.NewError( fmt.Sprintf("Could not set access type. Error %d", errno) )
     }
 
     if errno := C.snd_pcm_hw_params_set_format(playback, params, C.SND_PCM_FORMAT_FLOAT); errno < 0 {
-        return os.NewError( fmt.Printf("Could not set sample format. Error %d", errno) )
+        return os.NewError( fmt.Sprintf("Could not set sample format. Error %d", errno) )
     }
 
     if errno := C.snd_pcm_hw_params_set_rate(playback, params, header.SampleRate, 0); errno < 0 {
-        return os.NewError( fmt.Printf("Could not set sample rate. Error %d", errno) )
+        return os.NewError( fmt.Sprintf("Could not set sample rate. Error %d", errno) )
     }
 
     if errno := C.snd_pcm_hw_params_set_channels(playback, params, header.Channels); errno < 0 {
-        return os.NewError( fmt.Printf("Could not set channel count. Error %d", errno) )
+        return os.NewError( fmt.Sprintf("Could not set channel count. Error %d", errno) )
     }
 
     if errno := C.snd_pcm_hw_params(playback, params); errno < 0 {
-        return os.NewError( fmt.Printf("Could not set hardware parameters. Error %d", errno) )
+        return os.NewError( fmt.Sprintf("Could not set hardware parameters. Error %d", errno) )
     }
 
     C.snd_pcm_hw_params_free(params)
 
     if errno := C.snd_pcm_prepare(playback); errno < 0 {
-        return os.NewError( fmt.Printf("Could not prepare audio device for use. Error %d", errno) )
+        return os.NewError( fmt.Sprintf("Could not prepare audio device for use. Error %d", errno) )
     }
 
+}
+
+//this one is slightly different
+//note the change in scope
+func (self *AlsaSource) prepare() {
+
+    var capture *C.snd_pcm_t
+    var params *C.snd_pcm_hw_params_t
+
+    if errno := C.snd_pcm_open(&capture, C.CString("default"), C.SND_PCM_STREAM_CAPTURE, 0); errno < 0 {
+        return os.NewError( fmt.Sprintf("Could not open device. Error %d", errno) )
+    }
+
+    defer C.snd_pcm_close(capture)
+
+    if errno := C.snd_pcm_hw_params_malloc(&params); errno < 0 {
+        return os.NewError( fmt.Sprintf("Could not allocate hardware parameters. Error %d", errno) )
+    }
+
+    if errno := C.snd_pcm_hw_params_any(capture, params); errno < 0 {
+        return os.NewError( fmt.Sprintf("Could not initialize hardware parameter structure. Error %d", errno) )
+    }
+
+    if errno := C.snd_pcm_hw_params_set_access(capture, params, C.SND_PCM_ACCESS_RW_INTERLEAVED); errno < 0 {
+        return os.NewError( fmt.Sprintf("Could not set access. Error %d", errno) )
+    }
+
+    if errno := C.snd_pcm_hw_params_set_format(capture, params, C.SND_PCM_FORMAT_FLOAT); errno < 0 {
+        return os.NewError( fmt.Sprintf("Could not set sample format. Error %d", errno) )
+    }
+
+    if errno := C.snd_pcm_hw_params_set_rate(capture, params, header.SampleRate, 0); errno < 0 {
+        return os.NewError( fmt.Sprintf("Could not set sample rate. Error %d", errno) )
+    }
+
+    if errno := C.snd_pcm_hw_params_set_channels(capture, params, 1); errno < 0 {
+        return os.NewError( fmt.Sprintf("Could not set channel count. Error %d", errno) )
+    }
+
+    if errno := C.snd_pcm_hw_params(capture, params); errno < 0 {
+        return os.NewError( fmt.Sprintf("Could not set parameters. Error %d", errno) )
+    }
+
+    C.snd_pcm_hw_params_free(params)
+
+    if errno := C.snd_pcm_prepare(capture); errno < 0 {
+        return os.NewError( fmt.Sprintf("Could not prepare audio interface for use. Error %d", errno) )
+    }
+
+    return nil
 }
