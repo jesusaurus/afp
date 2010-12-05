2b905df388e1bc52a19894fe1f63a03587916023
Aaron DeVore <aaron.devore@gmail.com>
2010-12-04 20:34:03 -0800

    Add a full copyright message to most files (not research/)

--- main/filterlist.go
+++ main/filterlist.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2010 Go Fightclub Authors
+// This source code is released under the terms of the
+// MIT license. Please see the file LICENSE for license details.
+
 package main
 
 import (
--- research/src/main.go
+++ research/src/main.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2010 Go Fightclub Authors
+// This source code is released under the terms of the
+// MIT license. Please see the file LICENSE for license details.
+
 package main;
 
 import (
@@ -20,7 +24,7 @@ func main() {
 */		for j,s := range sample {
 			if (j % 2 == 0) {
 				widt := 136
-				half := int16(width/2) 
+				half := int16(width/2)
 				offset := int16(float(s) * float(width) / 65535.0)
 
 				for i := 0; i < int(half + offset); i++ {
@@ -32,5 +36,5 @@ func main() {
 /*		println("Decoded ", l, " bytes, oh joy!")
 		println("Context shows a packet at: ", []int16(reflect.MakeSlice(unsafe.Pointer(context.Context.Outbuf))))*/
 	}
-	
+
 }
