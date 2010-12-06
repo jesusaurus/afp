cea602ff755678c560c5c06c9bd886ebf4cd3248
Eric O'Connell <eric.oconnell@gmail.com>
2010-12-06 01:32:16 -0800

    avoid deadlock by trying to read HeaderSource before available; small optimizations and clarity

---
+++ main/filterlist.go
@@ -0,0 +1,31 @@
+// Copyright (c) 2010 Go Fightclub Authors
+// This source code is released under the terms of the
+// MIT license. Please see the file LICENSE for license details.
+
+package main
+
+import (
+	"afp"
+	"afp/filters/null"
+	"afp/filters/fexec"
+	"afp/filters/stdout"
+	"afp/filters/libav"
+//	"./filters/delay"
+//	"./filters/ospipe"
+//	"./filters/demo"
+)
+
+var filters map[string]func() afp.Filter = map[string]func() afp.Filter {
+	"execsink"		: fexec.NewExecSink,
+	"execlink"		: fexec.NewExecLink,
+	"execsource"	: fexec.NewExecSource,
+	"nullsource"	: null.NewNullSource,
+	"nulllink"		: null.NewNullLink,
+	"nullsink"		: null.NewNullSink,
+	"stdoutsink"	: stdout.NewStdoutSink,
+	"libavsource"	: libavfilter.NewLibAVSource,
+//	"delay"  : delay.NewFilter,
+//	"stdin"  : ospipe.StdinSource,
+//	"stdout" : ospipe.StdoutSink,
+//	"nop"    : demo.NopFilter,
+}
