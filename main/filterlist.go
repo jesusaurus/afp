3ab6e3f8242c6e575ec2c04a4cd1c8d64d1aa41e
Cory Kolbeck <ckolbeck@cs.pdx.edu>
2010-12-05 23:33:15 -0800

    Draft format page

---
+++ main/filterlist.go
@@ -0,0 +1,25 @@
+// Copyright (c) 2010 Go Fightclub Authors
+// This source code is released under the terms of the
+// MIT license. Please see the file LICENSE for license details.
+
+package main
+
+import (
+	"afp"
+	"afp/filters/null"
+	"afp/filters/fexec"
+//	"./filters/delay"
+//	"./filters/ospipe"
+//	"./filters/demo"
+)
+
+var filters map[string]func() afp.Filter = map[string]func() afp.Filter {
+	"exec"   : fexec.NewFilter,
+	"nullsource" : null.NewNullSource,
+	"nulllink"   : null.NewNullLink,
+	"nullsink" : null.NewNullSink,
+//	"delay"  : delay.NewFilter,
+//	"stdin"  : ospipe.StdinSource,
+//	"stdout" : ospipe.StdoutSink,
+//	"nop"    : demo.NopFilter,
+}
