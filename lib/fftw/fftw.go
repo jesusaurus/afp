b0278e79c9875437029ca4c2dd452d6bd1436a84
Cory Kolbeck <ckolbeck@cs.pdx.edu>
2010-12-08 16:33:50 -0800

    fftw builds

--- lib/fftw/fftw.go
+++ lib/fftw/fftw.go
@@ -21,39 +21,50 @@ type fft_plan struct {
 	valid bool
 }
 
-type FFTPlan_r2r_1d_32 struct {
+type FFTPlan_r2r_1D_32 struct {
+	fft_plan
 	InBuff []float32
 	OutBuff []float32
 }
 
-type FFTPlan_r2c_1d_32 struct {
+type FFTPlan_r2c_1D_32 struct {
+	fft_plan
 	InBuff []float32
 	OutBuff []complex64
-	plan C.fftwf_plan
-	valid bool
 }
 
-func (self *FFTPlan32) Execute() {
+func (self *fft_plan) Execute() {
 	if !self.valid {
 		panic("Attempt to use destroyed FFT plan.")
 	}
 	C.fftwf_execute(self.plan)
 }
 
-func (self *FFTPlan32) Destroy() {
+func (self *fft_plan) Destroy() {
 	self.valid = false
 	C.fftwf_destroy_plan(self.plan)
 }
-func NewRealToComplexPlan_1D_32(data []float32, output []complex64, flags int) *FFTPlan32 {
-	return &FFTPlan32{C.fftwf_plan_dft_r2c_1d(C.int(len(data)), (*C.float)(&data[0]), 
-		(*C.fftwf_complex)(unsafe.Pointer(&output[0])),
-			C.uint(C.FFTW_UNALIGNED | flags)), true}
+
+//A note regarding the somewhat ugly cast from complex64 -> fftwf_complex
+//fftwf_complex is a float[2], which has the same memory layout as Go's complex64
+//The may seem brittle, but it's a standard leyout for complex numbers, and unlikely
+//to change.
+func NewRealToComplexPlan_1D_32(bufflen, flags int) *FFTPlan_r2c_1D_32 {
+	inBuff := make([]float32, bufflen)
+	outBuff := make([]complex64, bufflen / 2 + 1)
+
+	return &FFTPlan_r2c_1D_32{fft_plan{C.fftwf_plan_dft_r2c_1d(C.int(bufflen), (*C.float)(&inBuff[0]), 
+		(*C.fftwf_complex)(unsafe.Pointer(&outBuff[0])),
+				C.uint(C.FFTW_UNALIGNED | flags)), true}, inBuff, outBuff}
 }
 
-func NewRealToRealPlan_1D_32(data, output []float32, flags int, kind C.fftwf_r2r_kind) *FFTPlan32 {
+func NewFFTPlan_r2r_1D_32(bufflen, flags int, kind C.fftwf_r2r_kind) *FFTPlan_r2r_1D_32 {
+	inBuff := make([]float32, bufflen)
+	outBuff := make([]float32, bufflen)
 
-	return &FFTPlan32{C.fftwf_plan_r2r_1d(C.int(len(data)), (*C.float)(&data[0]),
-			           (*C.float)(&output[0]), kind, C.uint(C.FFTW_UNALIGNED | flags)), true}
+	return &FFTPlan_r2r_1D_32{fft_plan{C.fftwf_plan_r2r_1d(C.int(bufflen), (*C.float)(&inBuff[0]),
+				(*C.float)(&inBuff[0]), kind, C.uint(C.FFTW_UNALIGNED | flags)), true}, inBuff, outBuff}
+
 }
 
 /*
