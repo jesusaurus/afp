43e259af9258adb5c076c64c2d83cbd239db5b3d
Jonathan Harker <kjharke@cs.pdx.edu>
2010-12-15 21:22:48 -0800

    Scratch that double buffer. Merged bits of revision 252 into revision 108. (Yes, that is a rollback)

---
+++ main/main.go
@@ -0,0 +1,81 @@
+// Copyright (c) 2010 AFP Authors
+// This source code is released under the terms of the
+// MIT license. Please see the file LICENSE for license details.
+
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"afp/flags"
+	"sync"
+	"syscall"
+)
+
+const CHAN_BUF_LEN = 16
+
+var (
+	Pipeline []*FilterWrapper = make([]*FilterWrapper, 0, 100)
+	pipelineLock *sync.Mutex = &sync.Mutex{}
+
+
+	errors   *log.Logger      = log.New(os.Stderr, "[E] ", log.Ltime)
+	info     *log.Logger      = log.New(os.Stderr, "[I] ", log.Ltime)
+	verbose  bool
+	debugging bool
+	specFile string
+)
+
+func init() {
+	go SigHandler()
+}
+
+func main() {
+	mainArgs, pipespec := ParsePipeline(os.Args)
+	info.Printf("mainArgs = %v\n", mainArgs)
+	mainFlags := flags.FlagParser(mainArgs)
+	mainFlags.BoolVar(&verbose, "v", false, "Verbose output")
+	mainFlags.BoolVar(&debugging, "d", false, "Debugging mode - panics will not be caught.")
+	mainFlags.StringVar(&specFile, "f", "",
+		"Pull pipeline spec from a file rather than command line")
+	mainFlags.Parse()
+
+	if specFile != "" {
+		rawPipe, err := GetPipelineFromFile(specFile)
+		if err != nil {
+			errors.Println(err.String())
+			os.Exit(1)
+		}
+		_, pipespec = ParsePipeline(rawPipe)
+	}
+	InitPipeline(pipespec, verbose)
+	StartPipeline()
+
+	for _, filter := range Pipeline {
+		<-filter.finished
+		if verbose {
+			info.Printf("Filter '%s' finished", filter.name)
+		}
+	}
+}
+
+func SigHandler() {
+	for sig := range signal.Incoming {
+		usig, ok := sig.(signal.UnixSignal)
+
+		if !ok {
+			shutdown()
+			errors.Printf("Process received unknown signal: %s" + sig.String())
+			os.Exit(1)
+		}
+
+		if usig == syscall.SIGINT {
+			if verbose {
+				errors.Print("SIGINT received. Pipeline will terminate.\n")
+			}
+			shutdown()
+			os.Exit(1)
+		}
+	}
+}
---
+++ research/piping/open.go
@@ -0,0 +1,11 @@
+// Copyright (c) 2010 AFP Authors
+// This source code is released under the terms of the
+// MIT license. Please see the file LICENSE for license details.
+
+package main
+
+import "fightclub"
+
+func main() {
+    fightclub.Open("test.in")
+}
