cb71145082974c46d4f79b8d6e6c0f1a32825e0d
Jonathan Harker <kjharke@cs.pdx.edu>
2010-12-15 21:41:07 -0800

    I thought I fixed that bug... silly rollbacks.

---
+++ main/testmain.go
@@ -0,0 +1,38 @@
+// Copyright (c) 2010 AFP Authors
+// This source code is released under the terms of the
+// MIT license. Please see the file LICENSE for license details.
+
+package main
+
+import (
+//	"afp"
+//	"os/signal"
+	"log"
+	"os"
+	)
+
+var pipespec [][]string = [][]string{{"libavsource","-i","/tmp/test.mp3"},{"stdoutsink"}}
+
+const CHAN_BUF_LEN = 16
+
+var (
+	Pipeline []*FilterWrapper = make([]*FilterWrapper, 0, 100)
+
+	errors *log.Logger = log.New(os.Stderr, "[E] ", log.Ltime)
+	info *log.Logger = log.New(os.Stderr, "[I] ", log.Ltime)
+	verbose bool = false
+	)
+
+
+func main() {
+	InitPipeline(pipespec, verbose)
+	StartPipeline()
+
+	for _, filter := range Pipeline {
+		<-filter.finished
+		if verbose {
+			info.Printf("Filter '%s' finished", filter.name)
+		}
+	}
+}
+
