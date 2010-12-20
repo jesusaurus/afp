31cad5138257cd5c7897797018f285aed71102b9
Jonathan Harker <kjharke@cs.pdx.edu>
2010-12-20 04:08:23 -0800

    The beginnings of an echo/reverb filter.

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+#Copyright (c) 2010 AFP Authors
+#This source code is released under the terms of the
+#MIT license. Please see the file LICENSE for license details.
+
+echo "####### Building Libs #######"
+cd lib
+./build-libs
+echo "###### Building Filters #####"
+cd ../filters
+./build-filters
+echo "####### Building Main #######"
+cd ../main
+make	
+echo "##### Building Manpages #####"
+cd ../doc
+make
---
+++ lib/libav/libav.h
@@ -0,0 +1,86 @@
+// Copyright (c) 2010 AFP Authors
+// This source code is released under the terms of the
+// MIT license. Please see the file LICENSE for license details.
+
+
+/*
+ * libav.h
+ * Eric O'Connell
+ *
+ * Reduce the surface area of libav* to something more suited to our purposes
+ *
+ */
+
+#ifndef __LIBAV_H__
+#define __LIBAV_H__
+
+#include <libavformat/avformat.h>
+#include <libavcodec/avcodec.h>
+
+/**
+ * General information about a stream
+ *
+ * FIXME: elaborate
+ */
+
+typedef struct {
+	int32_t Channels;
+	int32_t Sample_size;
+	int32_t Sample_rate;
+	int64_t Content_length;
+	int32_t Frame_size; 
+} AVStreamInfo;
+
+/**
+ * A bunch of state to be passed around for the duration of a decode
+ *
+ * FIXME: elaborate
+ */
+
+typedef struct {
+	AVCodec *Codec;
+	AVFormatContext *Fctx;
+	AVCodecContext *Cctx;
+	int16_t *Outbuf;
+	int32_t Buf_size, Buf_len;
+	int8_t first_frame_used;
+	AVStreamInfo Info;
+	AVPacket Packet;
+} AVDecodeContext;
+
+/**
+ * modified from libavcodec/mpegaudio.h
+ *
+ * fast header check for resync
+ */
+static inline int ff_mpa_check_header(uint32_t header){
+    /* header */
+    if ((header & 0xffe00000) != 0xffe00000)
+        return -1;
+    /* layer check */
+    if ((header & (3<<17)) == 0)
+        return -1;
+    /* bit rate */
+    if ((header & (0xf<<12)) == 0xf<<12)
+        return -1;
+    /* frequency */
+    if ((header & (3<<10)) == 3<<10)
+        return -1;
+    return 0;
+}
+
+/**
+ * modified from libavutil/intreadwrite.h
+ *
+ * does "fun" byt order conversions
+ */
+
+#ifndef AV_RB32
+#   define AV_RB32(x)                           \
+    ((((const uint8_t*)(x))[0] << 24) |         \
+     (((const uint8_t*)(x))[1] << 16) |         \
+     (((const uint8_t*)(x))[2] <<  8) |         \
+      ((const uint8_t*)(x))[3])
+#endif
+
+#endif
