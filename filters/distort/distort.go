84a2f64c74277613a5eb47c3d32006b1064d88eb
Cory Kolbeck <ckolbeck@cs.pdx.edu>
2010-12-21 10:42:36 -0800

    Distort: Implement cubic clipping, begin variable clipping

--- filters/distort/distort.go
+++ filters/distort/distort.go
@@ -12,44 +12,49 @@ import (
 
 type DistortFilter struct {
 	ctx *afp.Context
-    gain, clip Float32
+    gain, clip, hardness Float32
     clipper func(*DistortFilter)
 }
 
 var clipTypes = map[string]func(*DistortFilter) {
     "hard" : hard,
-	"soft" : nil,
-	"overflow" : nil,
+	"variable" : variable,
+	"cubic" : cubic,
 	"foldback" : foldback,
-
 }
 
 func (self *DistortFilter) Init(ctx *afp.Context, args []string) os.Error {
 	self.ctx = ctx
 
     fParse := flags.FlagParser(args)
-	fParse.Float32Var("g", 1.0,
-		"Signal gain to apply before clipping. Must be greater than 0.")
-    clipLevel := fParse.Float64("c", 1.0,
-        "The amplitude at which to clip the signal. Must be between 0 and 1.")
-    clipType := fParse.String("t", 
-		"soft", "The type of clipping used: hard, soft, overflow, or foldback.")
+	fParse.Float32Var(&self.gain, "g", 1.0,
+		"Signal gain to apply before clipping. Must be > 0.")
+    fParse.Float32Var(&self.clip, "c", 1.0,
+        "The amplitude at which to clip the signal. Must be in (0,1)")
+	fParse.Float32Var(&self.hardness, "k", 100,
+			"Clipping 'hardness' for the variable clipping filter. Must be" +
+			" in [0,\u221E), where 0 is no clipping and \u221E is hard clipping.")
+	clipType := fParse.String("t", 
+		"soft", "The type of clipping used: hard, variable, cubic, or foldback.")
 	
 	fParse.Parse()
 
-	if gain64 <= 0 {
+	if self.gain <= 0 {
 		return os.NewError("Gain must be greater than 0.")
 	}
-	self.gain = float32(gain64)
 
-	if clip64 > 1 || clip64 < 0{
+	if self.clip > 1 || self.clip < 0 {
 		return os.NewError("Clipping level must be between 0 and 1")
 	}
-	self.clip = float32(clip64)
+
 	self.clipper, ok := clipTypes[clipType]
 
 	if !ok {
 		return os.NewError("Clipping type must be one of: hard, soft, overflow, or foldback")
+	}
+
+	if clipper != variable && self.hardness < 0 {
+		return os.NewError("Hardness must be in [0,\u221E).")
 	}
 
 	return nil
@@ -111,6 +116,9 @@ func foldback(f *DistortFilter) {
 //Helper function for foldback
 //Computes the actual value of a sample
 func fold(sample, clip float32) float32 {	
+	
+	//A single fold may cause the signal to exceed the clip level 
+	//on the other side, so we may need to fold multiple times
 	for sample > clip || sample < -clip {
 		if sample > clip {
 			sample = 2 * clip - sample
@@ -118,8 +126,58 @@ func fold(sample, clip float32) float32 {
 			sample = clip + sample
 		}
 	}
+
 	return sample
 }
+
+func cubic(f *DistortFilter) {
+	for frame := range f.ctx.Source {
+		for slice := range frame {
+			for ch, sample := range slice {
+				frame[slice][ch] = cubicClip(sample * f.gain, f.clip)
+			}
+		}
+		self.ctx.Sink <- frame
+	}
+}
+
+
+//This algorithm found at:
+//https://ccrma.stanford.edu/~jos/pasp/Soft_Clipping.html#29299
+//       { -2/3 * clip        x <= -clip
+// out = {  x - x^3/3   -clip < x < clip
+//       {  2/3 * clip        x >= clip
+func cubicClip(sample, clip float32) float32 {
+	if sample >= clip {
+		sample = 0.66666666666666666666666 * clip
+	} else if sample <= -clip {
+		sample = -0.66666666666666666666666 * clip
+	} else {
+		sample = sample - sample * sample * sample / 3
+	}
+	return sample
+}
+
+func variable(f *DistortFilter) {
+	for frame := range f.ctx.Source {
+		for slice := range frame {
+			for ch, sample := range slice {
+				frame[slice][ch] = variableClip(f.hardness, sample * f.gain, f.clip)
+			}
+		}
+		self.ctx.Sink <- frame
+	}
+}
+
+func variableClip(k, gain, sample, clip float32) float32 {
+}
+
+//Provides a good enough approximation of atan
+//in [-1,1].  
+func fastAtan(x float32) float32 {
+	return x / (1 + .28 * x * x)
+}
+
 
 //Original C version by Alexander Kritov 
 //http://www.musicdsp.org/archive.php?classid=1#68  
