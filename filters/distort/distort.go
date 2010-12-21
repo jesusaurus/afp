cf65c2bf5e456345509d65107b40ce9c92cc4571
Cory Kolbeck <ckolbeck@cs.pdx.edu>
2010-12-20 17:20:26 -0800

    integerate changes made offline

--- filters/distort/distort.go
+++ filters/distort/distort.go
@@ -6,15 +6,28 @@ package distort
 
 import (
 	"afp"
+	"afp/flags"
 	"math"
 )
 
 type DistortFilter struct {
 	ctx *afp.Context
+    amp Float32
+    clipper func(*DistortFilter)
+}
+
+var clipTypes = map[string]func(*DistortFilter) {
+    "hard" : hardCutoff,
 }
 
 func (self *DistortFilter) Init(ctx *afp.Context, args []string) os.Error {
 	self.ctx = ctx
+
+    fParse := flags.FlagParser(args)
+    amp64 := fParse.Float64("a", math.NaN(),
+        "The amplitude at which to clip the signal. 0.0 < a < 1.0")
+    clipT := fParse.String("t", "hard", "The type of clipping used: hard")
+
 	return nil
 }
 
@@ -27,4 +40,21 @@ func (self *DistortFilter) GetType() int {
 }
 
 func (self *DistortFilter) Start() {
+}
+
+//Original C version by Alexander Kritov 
+//http://www.musicdsp.org/archive.php?classid=1#68  
+func _DSF(x, a, N, fi float32) {
+    var (
+        s1 = pow(a, N-1.0) * sin((N - 1.0) * x + fi)
+        s2 = pow(a, N) * sin(N * x + fi)
+        s3 = a * sin(x + fi)
+        s4 = 1.0 - (2 * a * cos(x)) + (a * a)
+    )
+
+    if s4 == 0 {
+        return 0;
+    } else {
+        return (sin(fi) - s3 - s2 +s1) / s4;
+    }
 }
