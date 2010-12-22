9a0983dc4c1b6e29d7e11177171e228c1b119a3d
Eric O'Connell <eric.oconnell@gmail.com>
2010-12-22 14:39:49 -0800

    updated fftw_bench to be more 'realistic' by performing forward & backward, and using less exacting planning; tweaked makefile

--- lib/fftw/bench_fftw.go
+++ lib/fftw/bench_fftw.go
@@ -6,8 +6,9 @@ import (
 
 func main() {
 	for i := 0; i < 1000; i++ {
-		d := make([]float32, 5192)
-		_ = fftw.RealToReal1D_32(d, false, 3, fftw.EXHAUSTIVE, fftw.DHT)
+		d := make([]float32, 1152)
+		_ = fftw.RealToReal1D_32(d, false, 3, fftw.MEASURE, fftw.R2HC)
+		_ = fftw.RealToReal1D_32(d, false, 3, fftw.MEASURE, fftw.HC2R)
 		print(".")
 	}
 }
