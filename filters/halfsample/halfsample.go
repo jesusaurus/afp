f39d71692cee5b078b41a8a6fe533384178dcd91
Cory Kolbeck <ckolbeck@cs.pdx.edu>
2010-12-24 13:33:46 -0800

    Flag parsing for halfsample

--- filters/halfsample/halfsample.go
+++ filters/halfsample/halfsample.go
@@ -2,7 +2,7 @@
 // This source code is released under the terms of the
 // MIT license. Please see the file LICENSE for license details.
 
-package downsample
+package halfsample
 
 import (
 	"afp"
@@ -10,29 +10,45 @@ import (
 	"os"
 )
 
-type Downsampler struct {
+type Halfsampler struct {
 	ctx *afp.Context
+	downsampler func([][]float32) [][]float32
 }
 
-func (self *Downsampler) Init(ctx *afp.Context, args []string) os.Error {
+func (self *Halfsampler) Init(ctx *afp.Context, args []string) os.Error {
 	self.ctx = ctx
 
+	parser := flags.FlagParser(args)
+	linear := parser.Bool("linear", false, "Use a linear convolution before downsampling")
+	exp := parser.Bool("exp", false, "Use an exponential convolution before downsampling")
+	parser.Parse()
+
+	if linear == exp {
+		return os.NewError("You must specify exactly one convolution algorithm. " +
+			" Available choices are: linear, exp")
+	}
+
+	if linear {
+		self.downsampler = linearDS
+	} else {
+		self.downsampler = expDS
+	}
+}
+
+func (self *Halfsampler) Stop() os.Error {
 	return nil
 }
 
-func (self *Downsampler) Stop() os.Error {
-	return nil
+func (self *Halfsampler) GetType() int {
+	return afp.PIPE_LINK
 }
 
-func (self *Downsampler) GetType() int {
-	return afp.PIPE_< SOURCE | LINK | SINK >
-}
-
-func (self *Downsampler) Start() {
-	//The first thing Start should do is store
-	//and pass on the header info.
+func (self *Halfsampler) Start() {
 	header := <-self.ctx.HeaderSource
-	self.ctx.HeaderSink <- header
+	headerCopy := header
+	headerCopy.SampleRate = header.SampleRate / 2
+	headerCopy.FrameSize = header.FrameSize / 2
+	self.ctx.HeaderSink <- headerCopy
 
 	//Then process the content til there's no more to be had
 	for frame := range self.ctx.Source {
@@ -40,8 +56,8 @@ func (self *Downsampler) Start() {
 	}
 }
 
-func NewSkeleton() afp.Filter {
-	return &Downsampler{}
+func NewHalfsampler() afp.Filter {
+	return &Halfsampler{}
 }
 /*
 int filter_state;
@@ -54,6 +70,24 @@ void downsample( int *input_buf, int *output_buf, int output_count ) {
         output_sam = filter_state + ( input_buf[ input_idx++ ] >> 1 );
         filter_state = input_buf[ input_idx++ ] >> 2;
         output_buf[ output_idx++ ] = output_sam + filter_state;
+    }
+}
+*/
+
+/*
+int filter_state;
+
+void downsample( int *input_buf, int *output_buf, int output_count ) {
+    int input_idx, output_idx, input_ep1;
+    output_idx = 0;
+    input_idx = 0;
+    input_ep1 = output_count * 2;
+    while( input_idx < input_ep1 ) {
+        filter_state = ( filter_state + input_buf[ input_idx ] ) >> 1;
+        output_buf[ output_idx ] = filter_state;
+        filter_state = ( filter_state + input_buf[ input_idx + 1 ] ) >> 1;
+        input_idx += 2;
+        output_idx += 1;
     }
 }
 */
