ecba7e14d92908a82b0cb6a71ea9a4f49b28fc3e
Cory Kolbeck <ckolbeck@cs.pdx.edu>
2010-12-24 21:17:48 -0800

    More flag parsing and sanity checking in lfo

--- filters/lfo/lfo.go
+++ filters/lfo/lfo.go
@@ -8,19 +8,41 @@ import (
 	"afp"
 	"afp/flags"
 	"os"
+	"strings"
 )
 
 type LFOFilter struct {
 	ctx *afp.Context
+	amp, freq float32
+	mixchan int
 }
+
+var chArgToVal = map[string]int { "all" : -1, "left" : 0, "right" : 1 }
 
 func (self *LFOFilter) Init(ctx *afp.Context, args []string) os.Error {
 	self.ctx = ctx
 
 	parser := flags.FlagParser(args)
-	f := parser.Float32("f", 10, "The frequency of the signal to mix in (in Hz).")
-	a := parser.Float32("a", 0.5, "The amplitude of the signal to mix in.  Between 0 and 1.")
+	parser.Float32Var(&self.freq, "f", 10, "The frequency of the signal to mix in (in Hz).")
+	parser.Float32Var(&self.amp, "a", 0.5, "The amplitude of the signal to mix in.  Between 0 and 1.")
+	ch := parser.String("c", "left", "The channel to mix the signal into. May be all, left," +
+		" right, or an integer between 0 and the number of channels in the input signal.") 
 	parser.Parse()
+
+	if amp < 0 || amp > 1 {
+		return os.NewError("Amplitude must be between 0 and 1.")
+	}
+
+	chL := strings.ToLower(ch)
+
+	if mc, ok := chArgToVal[chL]; ok {
+		self.mixchan = mc
+	} else if mc, err := strconv.Atoi(chL); err == nil && mc >= 0 {
+		self.mixchan = mc
+	} else {
+		return os.NewError(fmt.Sprintf("Could not parse '%s' as a valid channel choice." +
+			" Must be one of: all, left, right, or an integer indicating the channel to be mixed with.", ch))
+	}
 
 	return nil
 }
@@ -34,6 +56,10 @@ func (self *LFOFilter) GetType() int {
 }
 
 func (self *LFOFilter) Start() {
+	
+}
+
+func (self *LFOFilter) mixAll() {
 	header := <-self.ctx.HeaderSource
 	self.ctx.HeaderSink <- header
 
@@ -41,6 +67,18 @@ func (self *LFOFilter) Start() {
 
 		self.ctx.Sink <- frame
 	}
+	
+}
+
+func (self *LFOFilter) mixOne() {
+	header := <-self.ctx.HeaderSource
+	self.ctx.HeaderSink <- header
+
+	for frame := range self.ctx.Source {
+
+		self.ctx.Sink <- frame
+	}
+	
 }
 
 func NewLFO() afp.Filter {
