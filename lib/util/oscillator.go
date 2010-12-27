26611e69ee03c61e15aaa144fd4363b584510605
Cory Kolbeck <ckolbeck@cs.pdx.edu>
2010-12-27 00:16:05 -0800

    Write fastSine func. Should be ok on [0,pi]

--- lib/util/oscillator.go
+++ lib/util/oscillator.go
@@ -5,8 +5,8 @@
 package util
 
 import (
-	"afp"
-	)
+	"math"
+)
 
 func GetTriangleOscillator(samplerate, freq, amp float32) (func() float32) {
 	period := samplerate / freq //Roughly, period in slices
@@ -46,8 +46,8 @@ func GetSawtootheOscillator(samplerate, freq, amp float32) (func() float32) {
 
 func GetSquareOscillator(samplerate, freq, amp float32) (func() float32) {
 	halfperiod := int(samplerate / (2 * freq)) //Roughly, period in slices
-	var val float32 = amp
-	var i := 0
+	val := amp
+	i := 0
 	
 	return func() float32 {
 		ret := val
@@ -61,3 +61,42 @@ func GetSquareOscillator(samplerate, freq, amp float32) (func() float32) {
 	}
 }
 
+func GetSineOscillator(samplerate, freq, amp float32) (func() float32) {
+	period := samplerate / freq //Roughly, period in slices
+	delta := period
+	var val float32 = 0
+
+	return func() float32 {
+		ret := val
+
+		val += delta
+
+		if val >= amp || val <= -amp {
+			delta = -delta
+		} 
+
+		return ret
+	}
+}
+
+//fastSine evaluates the first 5 terms in the Taylor series:
+//  sin(x) = x - x^3/3! + x^5/5! - x^7/7! + ...
+//This provides Good Enough(TM) accuracy in the first two quadrants
+//For [pi,2pi), we return -fastSine(x - pi)
+//
+//This function expects values only in the range [0,2pi)
+//Outside this range, it will misbehave terribly
+func fastSine(x float32) float32 {
+	if x > math.Pi {
+		return -fastSine(x - math.Pi)
+	}
+
+	//Calculate powers of x.
+	x_3 := x * x * x
+	x_5 := x_3 * x * x
+	x_7 := x_5 * x * x
+	x_9 := x_7 * x * x
+
+	return x - x_3/6 + x_5/120 - x_7/5040 + x_9/362880
+
+}
